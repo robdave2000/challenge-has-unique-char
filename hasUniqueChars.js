2c56f9fead794f256c4bb90420c413094c071c31
Robert Davis <robdave22@yahoo.com>
2021-11-03 10:30:36 -0400

    Works

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,26 @@
 // Write your code below
+function hasUniqueChar (stringCheck)
+{
+    stringCheck = stringCheck.toString();
+    
+    for (let i = 0; i < stringCheck.length; i++)
+    {
+        for ( let j = 0; j < stringCheck.length; j++)
+        {
+            if (i === j)
+            {
+
+            }
+            else if (stringCheck.charAt(i) === stringCheck.charAt(j))
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+console.log(hasUniqueChar("Monday"));
+console.log(hasUniqueChar("Moonday"));
+console.log(hasUniqueChar("Bob"));
